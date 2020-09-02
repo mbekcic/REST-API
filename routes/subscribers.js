80adce4a03438edb9c82674c141bf3e3ddedaf6f
mbekcic <177.profil@gmail.com>
2020-09-02 11:51:40 +0200

    Add subscriber schema

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Subscriber = require("../models/subscriber");
+
+//Getting all
+router.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+//Getting one
+router.get("/:id", (req, res) => {});
+
+//Creating one
+router.post("/", (req, res) => {});
+
+//Updating one
+router.patch("/:id", (req, res) => {});
+
+//Deleting One
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
