eb53836447dc9c5ab3436a3a3318858f58763c0e
mbekcic <177.profil@gmail.com>
2020-09-02 11:38:18 +0200

    Create routes

--- server.js
+++ server.js
@@ -10,4 +10,8 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
+
+const subscribersRouter = require("./routes/subscribers");
+app.use("/subscribers", subscribersRouter);
+
 app.listen(3000, () => console.log("Server started"));
