674dc924c0d693e28bc268c24f29f4d13a94b242
mbekcic <177.profil@gmail.com>
2020-09-06 10:27:54 +0200

    Getting one and Deleting one function

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Subscriber = require("../models/subscriber");
+const subscriber = require("../models/subscriber");
 
 //Getting all
 router.get("/", async (req, res) => {
@@ -13,7 +14,9 @@ router.get("/", async (req, res) => {
 });
 
 //Getting one
-router.get("/:id", (req, res) => {});
+router.get("/:id", getSubscriber, (req, res) => {
+  res.json(res.subscriber);
+});
 
 //Creating one
 router.post("/", async (req, res) => {
@@ -34,6 +37,27 @@ router.post("/", async (req, res) => {
 router.patch("/:id", (req, res) => {});
 
 //Deleting One
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({ message: "Deleted subscriber" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: "Cannot find subscriber" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
